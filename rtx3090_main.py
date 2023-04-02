607c2b5ac081b9599f9832c5c3b161cb103a8db3
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-04-02 21:55:10 +0800

    Updates on RL208 training.

--- rtx3090_main.py
+++ rtx3090_main.py
@@ -18,11 +18,11 @@ def train_depth():
     #           "--iteration=1")
 
 def test_depth():
-    os.system("python \"test_main.py\" --server_config=3 --img_to_load=1000 --plot_enabled=1 --network_version=\"depth_v01.14\" "
-              "--iteration=1")
+    # os.system("python \"test_main.py\" --server_config=3 --img_to_load=1000 --plot_enabled=1 --network_version=\"depth_v01.14\" "
+    #           "--iteration=1")
 
-    os.system("python \"test_main.py\" --server_config=3 --img_to_load=1000 --plot_enabled=1 --network_version=\"depth_v01.14\" "
-              "--iteration=2")
+    os.system("python \"test_main.py\" --server_config=3 --img_to_load=1000 --plot_enabled=1 --network_version=\"depth_v01.12\" "
+              "--iteration=6")
 
 def train_img2img():
     os.system("python \"train_img2img_main.py\" --server_config=3 --img_to_load=-1 "
