2d72b61fa859f4e0ed845527b111b056c854b4ec
Neil Del Gallego <neil.delgallego@dlsu.edu.ph>
2023-07-21 11:29:00 +0800

    Script for titan2 set.

--- titan2_main.py
+++ titan2_main.py
@@ -9,16 +9,16 @@ def train_depth():
     #           "--iteration=1")
 
     os.system("python3 \"train_main-iid.py\" --server_config=4 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.03_iid\" "
-              "--iteration=5")
-
-    os.system("python3 \"train_main-iid.py\" --server_config=4 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.04_iid\" "
+              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.07_iid\" "
               "--iteration=1")
 
     os.system("python3 \"train_main-iid.py\" --server_config=4 --img_to_load=-1 "
-              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.04_iid\" "
+              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.07_iid\" "
               "--iteration=5")
+
+    os.system("python3 \"train_main-iid.py\" --server_config=4 --img_to_load=-1 "
+              "--plot_enabled=0 --save_every_iter=500 --network_version=\"depth_v01.07_iid\" "
+              "--iteration=7")
 
 def test_depth():
     os.system("python3 \"test_main.py\" --server_config=4 --img_to_load=-1 --plot_enabled=1 --network_version=\"depth_v01.03\" "
@@ -39,8 +39,8 @@ def test_img2img():
               "--iteration=1")
 
 def main():
-    # train_depth()
-    test_depth()
+    train_depth()
+    # test_depth()
     # train_img2img()
     # os.system("shutdown /s /t 1")
 
